3a23f4e9af682ad0c5523196b69b74f62233d020
Ilia <ilia.klodin@student.griffith.ie>
2025-03-10 20:51:42 +0000

    Part Three - completed euroToDollar method. All tests passed.

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,8 +1,15 @@
 package griffith;
 
 public class Conversions {
+    /*
+     Completed method that converts the given euro amount to dollar amount with the conversion rate of 1.08.
+     If the passed euro amount is negative, it throws an IllegalArgumentException.
+     */
     public double euroToDollar(double euro) {
-        return 0.0;
+        if (euro < 0) {
+            throw new IllegalArgumentException("The currency amount can't be negative.");
+        }
+        return euro*1.08;
     }
 
     public double dollarToEuro(double dollar) {
